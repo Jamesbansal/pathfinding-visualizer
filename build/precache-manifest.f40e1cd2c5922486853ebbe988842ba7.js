b1871bb88619b1cde129dc642b10b79e191f2a8d
Abhay Bansal <54861701+Jamesbansal@users.noreply.github.com>
2021-10-08 22:10:19 +0530

    Add files via upload

---
+++ build/precache-manifest.f40e1cd2c5922486853ebbe988842ba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9a860c8c5e7c6a8bf71503e2e97876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68061edf79fa0d17a85",
+    "url": "/static/css/main.a0ab3751.chunk.css"
+  },
+  {
+    "revision": "a630968cba511403d833",
+    "url": "/static/js/2.8e683c52.chunk.js"
+  },
+  {
+    "revision": "c68061edf79fa0d17a85",
+    "url": "/static/js/main.d87449cd.chunk.js"
+  },
+  {
+    "revision": "512a4f888a379d4d7645",
+    "url": "/static/js/runtime-main.1638fc71.js"
+  }
+]);
